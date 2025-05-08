9e7e50462548fd9e0ac349310381e855344da0d7
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2025-05-08 18:08:41 +0700

    Ubah Nilai Content 3

--- app/src/main/java/com/uti/fragment_if23fx/MainActivity.kt
+++ app/src/main/java/com/uti/fragment_if23fx/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             // buat variabel bundle
             val bundle = Bundle()
             // kirim parameter bundle
-            bundle.putString("content3","Ini contoh Fragment 3")
+            bundle.putString("content3","Ini contoh Fragment 3 Dongo")
             fragment.arguments = bundle
 
             supportFragmentManager.beginTransaction().replace(R.id.frm_content, fragment).commit()
